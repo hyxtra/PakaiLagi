0e556d1e2b3fd3ef033a4feba4ed79f3126fa3ac
Petra Lovinno <66946271+hyxtra@users.noreply.github.com>
2026-09-18 07:07:37 +0700

    Delete src directory

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export { SUPABASE_URL, SUPABASE_ANON_KEY } from "./supabaseConfig";
-export { CATEGORIES } from "./categories";
-export { COLORS } from "./colors";
-export { ERROR_MESSAGES } from "./errorMessages";
--- src/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(timestamp: string): string {
-  const date = new Date(timestamp);
-  return date.toLocaleDateString("id-ID", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
-}
